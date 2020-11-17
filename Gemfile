44724d904e3905ebc1caefa7a490eb9277545aa2
Pedro Miranda <pbragamiranda@gmail.com>
2020-11-17 11:25:56 -0300

    restaurant app

--- Gemfile
+++ Gemfile
@@ -25,6 +25,9 @@ gem 'jbuilder', '~> 2.7'
 # use this to create seeds
 gem 'faker'
 
+# to create better forms
+gem 'simple_form'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #Create Read Update Delete
+  #verb 'path', to: 'controller#action'
+
+  # #See all the restaurants
+  # get '/restaurants', to: 'restaurants#index'
+
+  # #See details about a specif restaurant
+  # get '/restaurants/:id', to: 'restaurants#show'
+
+  # #Create a new restaurant
+  # get '/restaurants/new', to: 'restaurants#new' # to display the form
+  # post '/restaurants', to: 'restaurant#create'
+
+  # #Update a restaurant
+  # get '/restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  # #Delete a restaurant
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  resources :restaurants
+
 end
+
+# routes -> controller -> view
